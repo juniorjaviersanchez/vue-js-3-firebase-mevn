5ceeb30eda49baac8e37c4477c540a9ed642a711
jsanchezcust <jsanchez@suplos.com>
2022-03-14 18:32:38 -0500

    Fundamentos javasript

---
+++ 2-vue-js3-fundamentos/hola-mundo/main.js
@@ -0,0 +1,9 @@
+const app = Vue.createApp({
+    data() {
+        return{
+            title   : 'Mi Cuenta',
+            cantidad: 600,
+            enlace  : 'https://vuejs.org/guide/quick-start.html#without-build-tools'
+        }
+    }
+});
